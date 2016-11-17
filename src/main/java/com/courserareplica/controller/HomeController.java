2b41c9bd4c5d075c4adaa09daf74b1bf39db7c7e
Cicu Mihai <cicumihai02@gmail.com>
2016-11-18 00:33:33 +0200

    bootstrap navbar

--- src/main/java/com/courserareplica/controller/HomeController.java
+++ src/main/java/com/courserareplica/controller/HomeController.java
@@ -1,6 +1,7 @@
 package com.courserareplica.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
@@ -8,7 +9,8 @@ import org.springframework.web.bind.annotation.RequestMapping;
 public class HomeController {
 
 	@RequestMapping
-	public String home() {
+	public String home(Model m) {
+		m.addAttribute("activeNavButton", "home");
 		return "index";
 	}
 }
